9c90679a7edae653305427f8dcb53faa6db7a109
kathiyan7 <kathiyan1472@gmail.com>
2024-04-19 19:40:06 +0530

    basic output

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import './App.css'
 function App() {
   const steps = [
     {
-      id:'Great',
+      id:'intial',
       message:'Hello, Welcome to our website',
-      trigger:'Ask Name'
+      trigger:'Great'
     },
     {
-      id:'Ask Name',
+      id:'Great',
       message:'Please enter your name',
       trigger:'waiting1'
     },
@@ -47,25 +47,35 @@ function App() {
         {value:"Driving_License",label:"Driving License",trigger: "Driving_License"},
       ],
     },
-    // {
-    //   id:'Gov_Schemes',
-    //   options:[
-    //     {value:"First_education",label:"First_education",trigger:"First_education"},
-    //     {value:"Women_welfare",label:"Women_welfare",trigger:"Women_welfare"},
-    //     {value:"Pongal_Bonus",label:"Pongal_Bonus",trigger:"Pongal_Bonus"},
-    //     {value:"Flood_fund",label:"Flood_fund",trigger:"Flood_fund"},
-    //     {value:"Feild_fund",label:"Feild_fund",trigger:"Feild_fund"},
-    //   ],
-    // },
-    // {
-    //   id:'Gov_Exam_Application',
-    //   options:[
-    //     {value:"TNPSC_group_1",label:"TNPSC_group_1",trigger:"TNPSC_group_1"},
-    //     {value:"TNPSC_group_2",label:"TNPSC_group_2",trigger:"TNPSC_group_2"},
-    //     {value:"TNPSC_group_3",label:"TNPSC_group_3",trigger:"TNPSC_group_3"},
-    //     {value:"TNPSC_group_4",label:"TNPSC_group_4",trigger:"TNPSC_group_4"},
-    //   ],
-    // },
+    {
+      id:'Gov_Schemes',
+      message:'Select which Scheme you want to apply for',
+      trigger:'Schemes'
+    },
+    {
+      id:'Schemes',
+      options:[
+        {value:"First_education",label:"First_education",trigger:"First_education"},
+        {value:"Women_welfare",label:"Women_welfare",trigger:"Women_welfare"},
+        {value:"Pongal_Bonus",label:"Pongal_Bonus",trigger:"Pongal_Bonus"},
+        {value:"Flood_fund",label:"Flood_fund",trigger:"Flood_fund"},
+        {value:"Feild_fund",label:"Feild_fund",trigger:"Feild_fund"},
+      ],
+    },
+    {
+      id:'Gov_Exam_Application',
+      message:'Select which Exam you want to apply for',
+      trigger:'Application'
+    },
+    {
+      id:'Application',
+      options:[
+        {value:"TNPSC_group_1",label:"TNPSC_group_1",trigger:"TNPSC_group_1"},
+        {value:"TNPSC_group_2",label:"TNPSC_group_2",trigger:"TNPSC_group_2"},
+        {value:"TNPSC_group_3",label:"TNPSC_group_3",trigger:"TNPSC_group_3"},
+        {value:"TNPSC_group_4",label:"TNPSC_group_4",trigger:"TNPSC_group_4"},
+      ],
+    },
     // {
     //   id:'Gov_ID_Proof',
     //   message:'Thank you',
@@ -116,16 +126,61 @@ function App() {
       message:'You must be an Indian citizen with age above 18',
       trigger:'Great'
     },
+    // {
+    //   id:'Gov_Schemes',
+    //   message:'Thank you',
+    //   end:true
+    // },
     {
-      id:'Gov_Schemes',
-      message:'Thank you',
-      end:true
-    },
-    {
-      id:'Gov_Exam_Application',
-      message:'Thank you',
+      id:'First_education',
+      message:'You must be an Indian citizen',
       trigger:'Great'
     },
+    {
+      id:'Women_welfare',
+      message:'You must be an Indian citizen',
+      trigger:'Great'
+    },
+    {
+      id:'Pongal_Bonus',
+      message:'You must be an Indian citizen',
+      trigger:'Great'
+    },
+    {
+      id:'Flood_fund',
+      message:'You must be an Indian citizen',
+      trigger:'Great'
+    },
+    {
+      id:'Feild_fund',
+      message:'You must be an Indian citizen',
+      trigger:'Great'
+    },
+    {
+      id:'TNPSC_group_1',
+      message:'You must be an Indian citizen',
+      trigger:'Great'
+    },
+    {
+      id:'TNPSC_group_2',
+      message:'You must be an Indian citizen',
+      trigger:'Great'
+    },
+    {
+      id:'TNPSC_group_3',
+      message:'You must be an Indian citizen',
+      trigger:'Great'
+    },
+    {
+      id:'TNPSC_group_4',
+      message:'You must be an Indian citizen',
+      trigger:'Great'
+    },
+    // {
+    //   id:'Gov_Exam_Application',
+    //   message:'Thank you',
+    //   trigger:'Great'
+    // },
   ]
 
   return (
